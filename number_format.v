21fff1648a1bceae179c5dba5c4c974e789e885a
Dayananthan <thavadaya28@gmail.com>
2026-08-13 23:44:17 +0530

    sized number representation in verilog

---
+++ number_format.v
@@ -0,0 +1,21 @@
+by default numbers are decimal
+
+
+
+3'b010;
+8'b1111_0000;
+
+3'd2 //size = 3 bits , decimal, value = 2
+8'd234; //size = 8 bits , decimal , value = 234
+
+8'h70 // 8 bits value 0x70
+9'h1FA // 9 bits hex
+
+4'o12  // size 4 bits value octal 12 (decimal 10)
+
+4'hA = 4'd10 = 4'b1010 = 4'o12 // all are same because the bitsize is important if bitsize are different iam not sure the equality
+
+//lower case valid in the hexadecimal represnetation 16'hcafe
+
+//unsized numbers 
+
